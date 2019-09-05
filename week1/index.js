c08b5f844d628b69494c9c327d3a25c9ba6752dc
Max <maksimiliyan@messagebird.com>
2019-09-05 15:26:02 +0200

    minor fix

--- week1/index.js
+++ week1/index.js
@@ -43,9 +43,14 @@ async function app() {
   };
 
   // When clicking a button, add an example for that class.
-  document.getElementById('class-a').addEventListener('click', () => addExample(0));
-  document.getElementById('class-b').addEventListener('click', () => addExample(1));
-  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-a').addEventListener('click', () => addExample(1));
+  document.getElementById('class-b').addEventListener('click', () => addExample(2));
+  document.getElementById('class-c').addEventListener('click', () => addExample(3));
+
+  // Nothing at first
+  for (var i = 0; i < 10; i++) {
+    addExample(0);
+  }
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
